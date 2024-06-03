f30a4ab99d4eb1fce96e543af6cf29c22091da42
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2024-06-03 12:13:33 +0700

    learning OOP python-fixing bird

--- character.py
+++ character.py
@@ -1,15 +1,30 @@
 """"this is a class method for character"""
 
+# pylint: disable=line-too-long
+
 
 class Bird:
     """ini adalah class untuk bird"""
 
-    def __init__(self, color):
+    species = "burung"  # ini adalah class attribute
+
+    def __init__(self, color, name):  # ini adalah instance attribute
         self.color = color
+        self.name = name
+
+    def chirp(self):
+        """_summary_: ini adalah method untuk burung berkicau"""
+        print("tweet")
+
+    def fly(self, heights=10):
+        # kita bisa menetapkan default value di parameter, atau kita bisa memanggil method tanpa parameter
+        """_summary_: ini adalah method untuk burung terbang"""
+        print(f"{self.name} flies with {heights} height")
 
 
-my_bird = Bird("merah")
+my_bird = Bird("merah", "robin")
 print(my_bird.color)  # ini adalah instance attribute
 print(type(my_bird))  # ini adalah instance type
 print(isinstance(my_bird, Bird))  # ini adalah instance type
 print(isinstance(my_bird, object))  # ini adalah instance type
+print(my_bird.species)  # ini adalah class attribute
